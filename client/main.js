019073f40d5ec1ed70697585e8fee98a332f59a6
Azamat7 <S.A.Smagulov@gmail.com>
2018-05-30 17:42:25 +0900

    added tuple views and connected simple firebase

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
+/*
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(5);
@@ -19,4 +19,30 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
+});*/
+
+$(document).ready(function(){
+  var config = {
+    apiKey: "AIzaSyCvXSpqkE4psy1qrS1-F2P3xcJjzySSRt0",
+    authDomain: "kaisttuple.firebaseapp.com",
+    databaseURL: "https://kaisttuple.firebaseio.com",
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  database.ref("tuples/").once("value").then(function(snapshot){
+  	let dbSnapshot = snapshot.val();
+    let keyVal = Object.keys(dbSnapshot);
+    for ( var i = 0; i < keyVal.length; i++) {
+      let tupleDescription = dbSnapshot[keyVal[i]];
+      let html = 
+      '<div class = "list-group-item list-group-secondary ' + keyVal[i] + '">' +
+        '<h5 class = "mb-1">'+ keyVal[i] + " " + '</h5>' + 
+        '<p class = "mt-2">' + tupleDescription + '</p>' + 
+        '<a class = "mt-3 btn btn-outline-info" href = "../map/map1.html" style = "font-size: 12px;"> More Description </a>'
+      '</div>';
+      $("#list-group-append").append(html);
+    }
+  });
 });
