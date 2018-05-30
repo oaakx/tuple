4ed9170592f49cedd6fea38df73c9a06c9173e2d
qvntm <32958247+qvntm@users.noreply.github.com>
2018-05-30 22:12:20 +0900

    Added Route for Tuple Description
    This shit took 2 hours. fml

--- server/main.js
+++ server/main.js
@@ -2,4 +2,13 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+	Router.route('/', function () {
+  	this.render('Home');
+	});
+	
+	Router.route('/map', function () { 
+  	// render the PageTwo template
+		this.layout('LayoutTwo');
+  	this.render('LayoutTwo'); 
+	});
 });
