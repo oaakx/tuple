9fa93fbb4cef47d4edc26aa3037280e40852cd91
Bolat Ashim <bolatashim@Bolats-MacBook-Air.local>
2018-06-01 11:34:18 +0900

    tweak

--- client/main.js
+++ client/main.js
@@ -20,3 +20,64 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+
+
+var config = {
+apiKey: "AIzaSyCvXSpqkE4psy1qrS1-F2P3xcJjzySSRt0",
+authDomain: "kaisttuple.firebaseapp.com",
+databaseURL: "https://kaisttuple.firebaseio.com",
+projectId: "kaisttuple",
+storageBucket: "kaisttuple.appspot.com",
+messagingSenderId: "623663672969"
+};
+firebase.initializeApp(config);
+
+
+var database = firebase.database();
+var refUser = database.ref("users");
+
+  //TODO: messages checking for validity of e-mail address
+  //      length of pass should be > 6
+
+
+  // const txtpassword = document.getElementById("password");
+  // const txtemail = document.getElementById("username");
+  // const btnlogin = document.getElementById("loginbtn");
+  // const btnregister = document.getElementById("registerbtn");
+
+  // //login event listener
+  // btnlogin.addEventListener("click", e => {
+  //   const email = txtemail.value;
+  //   const pass = txtpassword.value;
+  //   const auth = firebase.auth();
+  //   const promise = auth.signInWithEmailAndPassword(email, pass);
+  //   promise
+  //     .then(user => window.location="map.html")
+  //     .catch(e => console.log(e.message));
+  // });
+
+  // function addUser(user) {
+  //   refUser.push({
+  //     id: user.email,
+  //     point: 0,
+  //     group: "Daejeon",
+  //     lng: 50,
+  //     lat: 50
+  //   });
+
+  //   window.location="map.html"
+  // }
+
+  // //registration event listener
+  // btnregister.addEventListener("click", e => {
+  //   const email = txtemail.value;
+  //   const pass = txtpassword.value;
+  //   const auth = firebase.auth();
+
+  //   const promise = auth.createUserWithEmailAndPassword(email, pass);
+  //   promise
+  //     .then(user => addUser(user))
+  //     .catch(e => console.log(e.message));
+  // });
+
