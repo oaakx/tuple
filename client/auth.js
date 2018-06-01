99df12a9985b06d00df0f1c342dc03292d51b6bc
Bolat Ashim <bolatashim@Bolats-MacBook-Air.local>
2018-06-01 14:24:34 +0900

    added user authentication

---
+++ client/auth.js
@@ -0,0 +1,71 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import './auth.html';
+
+
+
+//REGISTER AND LOGIN FUNCTIONALITY HERE
+
+Router.route('/register', {
+	name: 'register',
+    template: 'register'
+});
+
+
+Router.route('/login', {
+	name: 'login',
+	template: 'login'
+});
+
+
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        }, function(error) {
+        	if(error) {
+        		console.log(error.reason);
+        	} else {
+        		Router.go("/"); //Need to enable going to the page where login_signup was triggered
+        	}
+        });
+    }
+});
+
+
+Template.bottomnavbar.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('login');
+    }
+});
+
+
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+		Meteor.loginWithPassword(email, password, function(error){
+		    if(error){
+		        console.log(error.reason);
+		    } else {
+		        Router.go("/");
+		    }
+		});
+    }
+});
+
+
+
+// Meteor.loginWithPassword(email, password, function(error){
+//     console.log(error);
+// });
+
+
+
