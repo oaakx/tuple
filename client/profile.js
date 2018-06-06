1aba2ed42d1b70f5a3bacbfed50b3c962f0a9687
qvntm <32958247+qvntm@users.noreply.github.com>
2018-06-06 14:22:12 +0900

    Added mytuples

--- client/profile.js
+++ client/profile.js
@@ -8,3 +8,21 @@ Template.profile.helpers({
     return Meteor.user().emails[0]["address"];
   }
 });
+
+Router.route('/mytuples',function(){
+  this.render("browsebar",{to:"header"});
+  this.render("mytuples");
+})
+
+Template.mytuples.helpers({
+  'tuples': function(){
+      var url = location.href;
+      if (!Meteor.user()) {
+        alert("You need to be logged in");
+        Router.go("login");
+        return false;
+      }
+      var user = Meteor.user().emails[0]["address"];
+      return tuplesList.find({creator: user}).fetch();
+  }
+});
