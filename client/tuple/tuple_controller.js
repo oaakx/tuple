c98627764530ce9e60157025dfb823c43e4d5e9e
Bolat Ashim <bolatashim@Bolats-MacBook-Air.local>
2018-06-07 23:04:37 +0900

    minor bug

--- client/tuple/tuple_controller.js
+++ client/tuple/tuple_controller.js
@@ -87,8 +87,7 @@ Template.tupleDescription.events({
   }
 });
 
-<<<<<<< HEAD:client/tuple/tuple_controller.js
-=======
+
 Template.createTuple.events({
   'click button'(event, instance) {
       let todayDate = new Date();
@@ -117,66 +116,3 @@ Template.createTuple.events({
   },
 });
 
-Template.commentbox.helpers({
-    'tup_comment': function(){
-        var url = location.href;
-        var tuple_id = url.substring(url.indexOf("tupleDescription")+17);
-        return Comments.find({tuple_id: tuple_id}).fetch();
-    }
-});
-
-Template.commentbox.events({
-  'click button':function(event){
-      event.preventDefault();
-      if (!Meteor.user()) {
-        alert("You need to be logged in");
-        Router.go("login");
-        return false;
-      }
-      var madeBy = Meteor.user().emails[0]["address"];
-      var comment = document.getElementById('comment_text').value;
-      var url = location.href;
-      var tuple_id = url.substring(url.indexOf("tupleDescription")+17);
-      var created_time = new Date();
-
-      var day = created_time.getDate();
-      if (day < 10){
-        day = "0" + day;
-      }
-      var month = created_time.getMonth();
-      if (month < 10){
-        month = "0" + month;
-      }
-
-      var year = created_time.getFullYear().toString().substring(2);
-
-      var hour = created_time.getHours();
-      if (hour < 10){
-        hour = "0" + hour;
-      }
-
-      var minutes = created_time.getMinutes();
-      if (minutes < 10){
-        minutes = "0" + minutes;
-      }
-
-      var time =  hour + ":"+ minutes;
-
-      var disp_time = day+"/"+month+"/"+year+ " " + time;
-
-      if (comment == "") {
-        alert("You need to be logged in");
-        return false;
-      } else {
-        Comments.insert({
-          comment: comment,
-          createdAt: disp_time,
-          madeBy: madeBy,
-          tuple_id: tuple_id,
-        });
-        document.getElementById('comment_text').value='';
-        return false;
-      }
-    }
-});
->>>>>>> 0f7b9b00991a782fb7d673ea927b3779a2c7e21c:client/tuple.js
