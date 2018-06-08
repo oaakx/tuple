e0635f40a8ab938c38405da645d3b69f5f6c8329
Silap Aliyev <sylapaliyev@gmail.com>
2018-06-08 18:00:25 +0900

    it never works lol

--- client/chat/message_view.js
+++ client/chat/message_view.js
@@ -1,14 +1,74 @@
-/* === All chatrooms === */
+import { Mongo } from 'meteor/mongo'
+
+/* === New Chatroom === */
 
 Router.route('/chat/user/:email', function() {
-  // redirect to proper chat
+  let friend_email = Route.current().params['email'];
+  let my_email = Meteor.user().emails[0].address;
+  let chatroom = ChatRooms.findOne({members: {'$all': [my_email, friend_email]}});
+
+  if (chatroom == undefined) {
+    console.log('chatroom not found...');
+    // ChatRooms.insert({members: [my_email, friend_email]}, (err, inserted) => {
+    //   let room_id = inserted;
+    //   console.log('new room created: ' + inserted)
+    //   Router.go('/chat/room/' + room_id);
+    // });
+  } else {
+    let room_id = chatroom._id;
+    console.log('going to room: ' + room_id);
+    Router.go('/chat/room/' + room_id);
+  }
 });
+
+Router.route('/chat/new', function() {
+  this.render("chat-topbar", {to: "header"});
+  this.render("new-chatroom");
+});
+
+Template['new-chatroom'].events({
+  'submit #new-chatroom': (event) => {
+    event.preventDefault();
+    
+    let target = event.target;
+    let friend_email = target['friend-email'].value;
+    let my_email = Meteor.user().emails[0].address;
+    let chatroom = ChatRooms.findOne({members: {'$all': [my_email, friend_email]}});
+
+    if (chatroom == undefined) {
+      ChatRooms.insert({members: [my_email, friend_email]}, (err, inserted) => {
+        let room_id = inserted;
+        console.log('new room created: ' + inserted)
+        Router.go('/chat/room/' + room_id);
+      });
+    } else {
+      let room_id = chatroom._id;
+      Router.go('/chat/room/' + room_id);
+    }
+  }
+});
+
+/* === All Chatrooms === */
 
 Template['all-chatrooms'].helpers({
   chatrooms: function() {
-    console.log(Meteor.user())
     let my_email = Meteor.user().emails[0].address;
-    let chatrooms = ChatRooms.find({members: {'$in': [my_email]}}).fetch()
+    let chatrooms = ChatRooms.find({members: {'$in': [my_email]}}).fetch();
+
+    // remove my email from chatroom members
+    chatrooms.forEach((chatroom, index, chatrooms) => {
+      members = [];
+      for (let index in chatroom.members) {
+        email = chatroom.members[index];
+        if (email != my_email) {
+          members.push(email)
+        }
+      }
+      
+      chatrooms[index].members = members;
+      chatrooms[index].title = members.join(', ');
+    });
+
     return chatrooms;
   }
 });
@@ -18,7 +78,7 @@ Router.route('/chat/rooms', function() {
   this.render("all-chatrooms");
 });
 
-/* === A chatroom === */
+/* === Single Chatroom === */
 
 Router.route('/chat/room/:room_id', function() {
   // messages = controller.get_messages();
@@ -30,6 +90,17 @@ Template.chatroom.helpers({
   chatroom: () => {
     let room_id = Router.current().params['room_id'];
     let chatroom = ChatRooms.findOne(room_id);
+    let my_email = Meteor.user().emails[0].address;
+    members = [];
+    for (let index in chatroom.members) {
+      email = chatroom.members[index];
+      if (email != my_email) {
+        members.push(email)
+      }
+    }
+    chatroom.members = members;
+    chatroom.title = members.join(', ');
+
     return chatroom;
   },
   messages: () => {
